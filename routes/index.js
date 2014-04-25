4f869e828496b38ff9f389a56ef5283b1961063c
Drew Koch <dkoch13@gmail.com>
2014-04-25 14:22:10 -0700

    reorganize file structure

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/style-guide', function(req, res) {
   res.render('style-guide', { title: 'Style Guide' });
 });
 
